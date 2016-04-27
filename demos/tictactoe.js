1e0913bce5b228125f7a0d88b50df980bd43bd38
Steven Chennault <schenn@gmail.com>
2016-04-26 18:16:09 -0700

    Start rebuilding tic tac toe to use the new system setup

--- demos/tictactoe.js
+++ demos/tictactoe.js
@@ -1,3 +1,31 @@
-/**
- * Created by schenn on 4/25/16.
- */
+var game = function(){
+  this.currentPlayer = 1;
+  this.player1Wins = 0;  //total wins
+  this.player2Wins = 0;
+  this.turnCounter = 0;  //turns taken
+
+  this.setup = function(canvas){
+    CanvasEngine.ResourceManager.setImagePath("tictacgraphics/");
+    CanvasEngine.ResourceManager.addSpriteSheet(
+      {
+        name: "ticTacSprites",
+        source: "tictacsprites.png",
+        height: 100,
+        width: 100,
+        sprites:[
+          "oToken", "xToken", "positionBack", "winStar"
+        ]
+      }
+    );
+    CanvasEngine.ResourceManager.addSpriteSheet({
+      name: "winningStar",
+      source: "starsprites.png",
+      height: 100,
+      width: 100
+    });
+
+    CanvasEngine.setup(canvas);
+    CanvasEngine.Screen.maximize();
+  };
+};
+
