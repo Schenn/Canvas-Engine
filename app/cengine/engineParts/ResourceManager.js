83caf3b906b4f0f1cd6ac27996e302976cc429ff
Steven Chennault <schenn@gmail.com>
2016-04-26 20:16:22 -0700

    Wait for images to load before using them

--- app/cengine/engineParts/ResourceManager.js
+++ app/cengine/engineParts/ResourceManager.js
@@ -3,7 +3,8 @@
 
   var sounds = {}, images={}, spriteSheets = {}, imagePath = "";
   var resourcesLoaded = {};
-
+  var onLoad;
+  var allAdded = false;
 
   var resourceManager = function(){
     this.Resources = {};
@@ -19,36 +20,58 @@
     this.addImage = function(name, path, load){
       var image = new Image();
       resourcesLoaded[name] = false;
-
+      var self = this;
       image.addEventListener("load", function(){
         resourcesLoaded[name] = true;
-        images[name] = image;
+        if(self.resourcesAreLoaded()){
+          onLoad();
+        }
       });
+      images[name] = image;
       image.addEventListener("load", load);
       image.src = imagePath +"/"+path;
     };
 
     this.addSpriteSheet = function(name, path, details){
+      spriteSheets[name] = new this.Resources.SpriteSheet(details);
       this.addImage(name, path, function(image){
-
-        spriteSheets[name] = new this.Resources.SpriteSheet(details, image);
+        spriteSheets[name].processSprites(image);
       });
 
     };
 
     this.getSpriteSheet = function(name){
-
+      return spriteSheets[name];
     };
 
     this.getImage = function(name){
-
+      return image[name];
     };
 
     this.getSound = function(name){
+      return sounds[name];
+    };
 
+    this.resourcesAreLoaded = function(){
+      if(allAdded) {
+        var resourceKeys = Object.keys(resourcesLoaded);
+        for (var i = 0; i < resourceKeys.length; i++) {
+          if (resourcesLoaded[resourceKeys[i]] === false) {
+            return false;
+          }
+        }
+        return true;
+      }
+    };
+
+    this.finishedAddingResources = function(){
+      allAdded = true;
+    };
+
+    this.onResourcesLoaded = function(callback){
+      onLoad = callback;
     };
   };
-
 
 
   CanvasEngine.ResourceManager = new resourceManager();
