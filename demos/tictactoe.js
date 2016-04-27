0fd3cba7d427a46939f9c45d67d9df208c6baf31
Steven Chennault <schenn@gmail.com>
2016-04-26 20:18:20 -0700

    Keep functional testing

--- demos/tictactoe.js
+++ demos/tictactoe.js
@@ -5,11 +5,15 @@ var game = function(){
   this.turnCounter = 0;  //turns taken
 
   this.setup = function(canvas){
-    CanvasEngine.ResourceManager.setImagePath("tictacgraphics/");
-    CanvasEngine.ResourceManager.addSpriteSheet(
+    CanvasEngine.setup(canvas);
+    CanvasEngine.Screen.maximize();
+
+    CanvasEngine.ResourceManager.setImagePath("tictacgraphics");
+    CanvasEngine.ResourceManager.onResourcesLoaded(function(){
+  //    tictactoe.resourcesLoaded();
+    });
+    CanvasEngine.ResourceManager.addSpriteSheet("ticTacSprites", "tictacsprites.png",
       {
-        name: "ticTacSprites",
-        source: "tictacsprites.png",
         height: 100,
         width: 100,
         sprites:[
@@ -17,15 +21,62 @@ var game = function(){
         ]
       }
     );
-    CanvasEngine.ResourceManager.addSpriteSheet({
-      name: "winningStar",
-      source: "starsprites.png",
+    CanvasEngine.ResourceManager.addSpriteSheet("winningStar", "starsprites.png",{
       height: 100,
       width: 100
     });
 
-    CanvasEngine.setup(canvas);
-    CanvasEngine.Screen.maximize();
+    CanvasEngine.ResourceManager.finishedAddingResources();
+
+    var gamePieces = [];
+
+    // Black background box
+    gamePieces[0] = {
+      type: "RECT",
+      height:canvas.height,
+      width: canvas.width,
+      fillStyle: "#000000"
+    };
+
+    gamePieces[1] = {
+      type: "LABEL",
+      text: "Loading...",
+      x: canvas.width/2,
+      y: canvas.height/2,
+      z_index: 1
+    };
+
+    CanvasEngine.addMap(gamePieces);
+    CanvasEngine.Screen.drawScreen();
   };
+
+  // We have to wait for our assets to finish loading before we can use them.
+  this.resourcesLoaded = function(){
+    CanvasEngine.EntityTracker.clearEntities();
+    var gamePieces = [];
+
+    var cHeight = CanvasEngine.Screen.height();
+    var cWidth = CanvasEngine.Screen.width();
+
+    // Black background box
+    gamePieces[0] = {
+      type: "RECT",
+      height:canvas.height,
+      width: canvas.width,
+      fillStyle: "#000000"
+    };
+
+    gamePieces[1] = {
+      type: "SPRITE",
+      spritesheet: "ticTacSprites",
+      height: 100,
+      width: 100,
+      defaultSprite: "oToken",
+      x:100
+    };
+
+    CanvasEngine.addMap(gamePieces, true);
+  }
 };
 
+window.tictactoe = new game();
