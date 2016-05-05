98d52189315e023499014ae599e7ccd3e6f5c128
Steven Chennault <schenn@gmail.com>
2016-05-04 17:55:45 -0700

    Cleanup and bug fix.

--- app/cengine/components/Movement.js
+++ app/cengine/components/Movement.js
@@ -1,28 +1,36 @@
 (function(){
   var props = CanvasEngine.EntityManager.properties;
   var utils = CanvasEngine.utilities;
-  var lastDirection;
 
   var movement = function(params, entity){
     var x = params.x,
       y = params.y,
-      xSpeed = params.xSpeed,
-      ySpeed = params.ySpeed;
+      xSpeed = utils.exists(params.xSpeed) ? params.xSpeed : 0,
+      ySpeed = utils.exists(params.ySpeed) ? params.ySpeed : 0;
+
+    var lastDirection;
 
     if(utils.isFunction(params.onDirectionChange)){
       this.onDirectionChange = params.onDirectionChange.bind(this);
     }
 
+    Object.defineProperties(this, {
+      "x": props.defaultProperty(x,params.onMoveX),
+      "y": props.defaultProperty(y, params.onMoveY)
+    });
+
     this.move = function(delta){
       if(delta > 0){
-        x += xSpeed * delta / 1000;
-        y += ySpeed * delta / 1000;
+        this.x += (xSpeed > 0) ? xSpeed * delta : 0;
+        this.y += (ySpeed > 0) ? ySpeed * delta : 0;
       } else {
-        x += xSpeed;
-        y += ySpeed;
+        this.x += (xSpeed > 0) ? xSpeed : 0;
+        this.y += (ySpeed > 0) ? ySpeed : 0;
       }
-      if(this.getDirection() !== lastDirection && utils.isFunction(this.onDirectionChange)){
-        this.onDirectionChange(this.getDirection());
+      if(this.getDirection().direction !== lastDirection && utils.isFunction(this.onDirectionChange)){
+        var dir = this.getDirection();
+        this.onDirectionChange(dir);
+        lastDirection = dir.direction;
       }
     };
 
@@ -32,8 +40,8 @@
     };
 
     this.setOrigin = function(coords){
-      x = coords.x;
-      y = coords.y;
+      this.x = coords.x;
+      this.y = coords.y;
     };
 
     this.getEntity = function(){
@@ -41,32 +49,15 @@
     };
 
     this.asObject = function(){
-      return $.extend({}, {x:x, y:y,xSpeed: xSpeed, ySpeed: ySpeed});
+      return $.extend({}, {x:this.x, y:this.y,xSpeed: xSpeed, ySpeed: ySpeed});
     };
 
     this.getDirection = function () {
       var direction = "";
-      switch (ySpeed){
-        case ySpeed < 0:
-          direction = "N";
-          break;
-        case ySpeed > 0:
-          direction = "S";
-          break;
-        default:
-          break;
-      }
 
-      switch (xSpeed){
-        case xSpeed < 0:
-          direction += "W";
-          break;
-        case xSpeed > 0:
-          direction += "E";
-          break;
-        default:
-          break;
-
+      direction += (ySpeed > 0) ? "S" : "N";
+      if(xSpeed !== 0) {
+        direction += (xSpeed > 0) ? "E" : "W";
       }
 
       return {
