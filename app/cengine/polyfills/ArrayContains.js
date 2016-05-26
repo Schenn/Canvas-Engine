ef8d578c384b203744c11fe790fc0c13c6d8d7d8
Steven Chennault <schenn@gmail.com>
2016-05-25 22:36:39 -0700

    Added polyfill for array.includes

--- app/cengine/polyfills/ArrayContains.js
+++ app/cengine/polyfills/ArrayContains.js
@@ -1,3 +1,28 @@
-/**
- * Created by schenn on 5/25/16.
- */
+if (!Array.prototype.includes) {
+  Array.prototype.includes = function(searchElement /*, fromIndex*/ ) {
+    'use strict';
+    var O = Object(this);
+    var len = parseInt(O.length) || 0;
+    if (len === 0) {
+      return false;
+    }
+    var n = parseInt(arguments[1]) || 0;
+    var k;
+    if (n >= 0) {
+      k = n;
+    } else {
+      k = len + n;
+      if (k < 0) {k = 0;}
+    }
+    var currentElement;
+    while (k < len) {
+      currentElement = O[k];
+      if (searchElement === currentElement ||
+        (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
+        return true;
+      }
+      k++;
+    }
+    return false;
+  };
+}
