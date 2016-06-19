76cc974750e1b21e4e5c4abccb97a84650586494
Steven Chennault <schenn@gmail.com>
2016-06-18 19:11:38 -0700

    Clean up jQuery functions to be more 'plugin' like

--- app/cengine/jqueryPlugins/DOM/Maximize.js
+++ app/cengine/jqueryPlugins/DOM/Maximize.js
@@ -13,6 +13,10 @@
 
   /**
    * Enlarge a canvas or image to a size relative to it's parent container
+   * @example
+   *  // make 50% of parent container
+   * $('canvas').maximize(50);
+   *
    * @memberof external:jQuery.fn
    * @alias maximize
    *
@@ -21,8 +25,10 @@
   $.fn.maximize = function(modifier){
     // Divide the modifier by 100, if there is no modifier set it to the default
     mod = typeof(modifier) != 'undefined' ? modifier / 100 : mod;
-    var height = parseInt(this.parent().height()) * mod;
-    var width = parseInt(this.parent().width()) * mod;
-    this.attr("height", height).attr("width", width);
+    this.attr("height",
+      Math.ceil(this.parent().height() * mod)
+    ).attr("width",
+      Math.ceil(this.parent().width() * mod)
+    );
   };
 })(jQuery);
--- app/cengine/jqueryPlugins/DOM/addZLayer.js
+++ app/cengine/jqueryPlugins/DOM/addZLayer.js
@@ -7,18 +7,17 @@
    * Add a Canvas to the top of the stack.
    *  Sets the z-index of the canvas so that if they are absolutely positioned, they actually stack.
    *
-   * @param {number} height
-   * @param {number} width
    * @param {number} z
    * @memberof external:jQuery.fn
    * @alias addZLayer
    * @returns {HTMLElement}
    */
-  $.fn.addZLayer = function(height, width, z){
+  $.fn.addZLayer = function(z){
     var tag = 'zLayer'+z;
+
     var canvas = $("<canvas id='" + tag + "'></canvas>");
     canvas.appendTo(this.parent());
-    canvas.attr("height", height).attr("width", width).css("z-index", z);
+    canvas.attr("height", this.height()).attr("width", this.width()).css("z-index", z);
     return canvas;
   };
 })(jQuery);
