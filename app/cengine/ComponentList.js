f6f77138cd091d1b0e6d9e588b0deeb3bd65e925
Steven Chennault <schenn@gmail.com>
2016-06-24 21:24:05 -0700

    Create the master list of components. This is used by Entitymanager to create new components.

--- app/cengine/ComponentList.js
+++ app/cengine/ComponentList.js
@@ -2,52 +2,48 @@
  * Created by schenn on 6/23/16.
  */
 // Base Component
+import Component from "components/Component.js"
 
-
-// Primary Components
-import Image from "components/Image.js"
+// Renderer Components
 import Renderer from "components/Renderer.js"
-import SpriteSheet from "components/SpriteSheet.js"
+
+// Functional Components
+import Timer from "components/Timer.js"
+import PointPlotter from "components/PointPlotter.js"
+import Movement from "components/Movement.js"
+
+// Data Wrapper Components
+import ImageWrapper from "components/ImageWrapper.js"
+import SpriteSheetWrapper from "components/SpriteSheetWrapper.js"
 import Text from "components/Text.js"
 import TileMap from "components/TileMap.js"
 
+// UI Components
 import KeyPress from "components/KeyPress.js"
 import Mouse from "components/Mouse.js"
-import Movement from "components/Movement.js"
-import PointPlotter from "components/PointPlotter.js"
 
-import Timer from "components/Timer.js"
+
 
 
 export default function getComponentList(customComponents = new Map()) {
   var componentList = new Map();
-  componentList.set("_", new Map());
-  let _ = componentList.get("_");
-  _.set("AnimatedSprite", AnimatedSprite);
-  _.set("Image",Image);
-  _.set("Label",Label);
-  _.set("Line",Line);
-  _.set("MobileSprite",MobileSprite);
-  _.set("Rect",Rect);
-  _.set("Sprite",Sprite);
-  _.set("TileMap",TileMap);
 
-  componentList.set("functional", new Map());
-  let functional = componentList.get("functional");
-  functional.set("Animator", Animator);
+  componentList.set("BaseComponent", Component);
 
-  componentList.set("ui", new Map());
-  let ui = componentList.get("ui");
-  ui.set("Button", Button);
+  componentList.set("Renderer", Renderer);
+  componentList.set("Timer", Timer);
+  componentList.set("PointPlotter", PointPlotter);
+  componentList.set("Movement", Movement);
 
+  componentList.set("Image", ImageWrapper);
+  componentList.set("SpriteSheet", SpriteSheetWrapper);
+  componentList.set("Text", Text);
+  componentList.set("TileMap", TileMap);
+  componentList.set("KeyPress", KeyPress);
+  componentList.set("Mouse", Mouse);
 
-  // { 'namespace|name': class reference }
-  customEntities.forEach(function(fullName, classRef){
-    let name = fullName.split("|");
-    if(!componentList.has(name[0])){
-      componentList.set(name[0], new Map());
-    }
-    componentList.get(name[0]).set(name[1], classRef);
+  customComponents.forEach(function(name, classRef){
+    componentList.set(name, classRef);
 
   });
 
