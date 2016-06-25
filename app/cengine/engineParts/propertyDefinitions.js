9dacc70ac48c663ba25ef9361a5b2407f71b6206
Steven Chennault <schenn@gmail.com>
2016-06-24 21:24:45 -0700

    Utilities is now a class. PropertyDefinitions is its own fixed class.
    PropertyDefinitions contains the different property definitions for
    canvasengine properties. It also includes a method for observing a
    variable and triggering a callback when the value is changed. It also
    contains a method for creating a simple proxy for an object or array.

--- app/cengine/engineParts/propertyDefinitions.js
+++ app/cengine/engineParts/propertyDefinitions.js
@@ -1,3 +1,70 @@
 /**
  * Created by schenn on 6/23/16.
  */
+let propertyDefinitions = {
+  defaultProperty: function (privateVar, callback) {
+    return {
+      enumerable: true,
+      configurable: false,
+      get: function () {
+        return privateVar;
+      },
+      set: function (val) {
+        if(typeof(privateVar)=="number"){
+          val = Number(val);
+        } else if (typeof(privateVar) == "string"){
+          val = val.toString();
+        } else if(typeof(privateVar) == "boolean"){
+          val = Boolean(val);
+        }
+        privateVar = val;
+        if(CanvasEngine.utilities.isFunction(callback)){
+          callback(val);
+        }
+      }
+    };
+  },
+  lockedProperty: function (privateVar, callback) {
+    return {
+      enumerable: true,
+      configurable: false,
+      get: function () {
+        return privateVar;
+      },
+      set: function (val) {
+        if(typeof(privateVar)=="number"){
+          val = Number(val);
+        } else if (typeof(privateVar) == "string"){
+          val = val.toString();
+        } else if(typeof(privateVar) == "boolean"){
+          val = Boolean(val);
+        }
+        privateVar = val;
+        if(CanvasEngine.utilities.isFunction(callback)){
+          callback(val);
+        }
+      }
+    };
+  },
+  proxy: function(privateVar){
+    return new Proxy(privateVar, {
+      get: function(privateVar, index){
+        return privateVar[index];
+      }
+    });
+  },
+  observe: function({name, value, locked=false, callback}, object){
+    var prop = (locked == true)? this.lockedProperty(value, callback) : this.defaultProperty(value, callback);
+    Object.defineProperty(object, name, prop)
+  }
+};
+
+var properties = new Proxy(propertyDefinitions, {
+  get: function(definitions, name){
+    return definitions[name];
+  }
+});
+
+export default properties;
+
+
--- app/cengine/engineParts/utilities.js
+++ app/cengine/engineParts/utilities.js
@@ -1,132 +1,65 @@
-(function(CanvasEngine){
-  /**
-   * A collection of utility functions
-   *
-   * @namespace utilities
-   * @memberof CanvasEngine
-   * @inner
-   *
-   */
-  var utilities = {};
+export function exists(val){
+  return typeof (val) !== "undefined" && val !== null;
+}
 
-  /**
-   * Does a value actually exist?
-   * @param {*} val
-   * @memberof CanvasEngine~utilities
-   * @returns {boolean}
-   */
-  utilities.exists=function(val){
-    return typeof (val) !== "undefined" && val !== null;
-  };
+export function orDefault(potential, def){
+  return exists(potential) ? potential : def;
+}
 
-  /**
-   * Return A value or another
-   * @param {*} potential The potential value
-   * @param {*} def The default value if no potential
-   * @memberof CanvasEngine~utilities
-   * @returns {*}
-   */
-  utilities.orDefault = function(potential, def){
-    return this.exists(potential) ? potential : def;
-  };
+export function isFunction(arg){
+  return exists(arg) && $.isFunction(arg);
+}
 
-  /**
-   * Is the given value a function?
-   * @param {*} prop
-   * @memberof CanvasEngine~utilities
-   * @returns {boolean}
-   */
-  utilities.isFunction = function(prop){
-    return this.exists(prop) && $.isFunction(prop);
-  };
+export function isArray(arg){
+  return (!!arg) && (arg.constructor === Array);
+}
 
-  /**
-   * Is the given value an Array?
-   * @param {*} prop
-   * @memberof CanvasEngine~utilities
-   * @returns {boolean}
-   */
-  utilities.isArray = function(prop){
-    return (!!prop) && (prop.constructor === Array);
-  };
-
-  /**
-   * Clean an array of its empty indexes.
-   *    Deleting a position from an array doesn't also remove the index.
-   *
-   * @param {Array} cleanMe The array to clean up
-   * @memberof CanvasEngine~utilities
-   * @returns {Array}
-   */
-  utilities.cleanArray = function(cleanMe){
-    var cleaner = [];
+export function cleanArray(cleanMe){
+  let cleaner = [];
+  if(isArray(cleanMe)) {
     for (var i = 0; i < cleanMe.length; i++) {
-      if (this.exists(cleanMe[i])) {
+      if (exists(cleanMe[i])) {
         cleaner.push(cleanMe[i]);
       }
     }
     return cleaner;
-  };
+  } else {
+    console.log(cleanMe + " - is not an array");
+    return cleanMe;
+  }
+}
 
-  /**
-   * Generate a random string to name something
-   *
-   * @memberof CanvasEngine~utilities
-   * @returns {string}
-   */
-  utilities.randName = function () {
-    var length = 8 + Math.floor(7 * (Math.random() % 1));
-    var val = "ce_";
-    for (var i = 1; i <= length; i++) {
-      var slots = 1 + Math.floor(4 * (Math.random() % 1));
-      switch (slots) {
-        case 1:
-          val += 48 + Math.floor(10 * (Math.random() % 1));
-          break;
-        case 2:
-          val += String.fromCharCode(65 + Math.floor(26 * (Math.random() % 1)));
-          break;
-        case 3:
-          val += String.fromCharCode(97 + Math.floor(26 * (Math.random() % 1)));
-          break;
-      }
+export function randName() {
+  let length = 8 + Math.floor(7 * (Math.random() % 1));
+  let val = "ce_";
+  for (let i = 1; i <= length; i++) {
+    let slots = 1 + Math.floor(4 * (Math.random() % 1));
+    switch (slots) {
+      case 1:
+        val += 48 + Math.floor(10 * (Math.random() % 1));
+        break;
+      case 2:
+        val += String.fromCharCode(65 + Math.floor(26 * (Math.random() % 1)));
+        break;
+      case 3:
+        val += String.fromCharCode(97 + Math.floor(26 * (Math.random() % 1)));
+        break;
     }
-    return (val);
-  };
+  }
+  return (val);
+}
 
-  /**
-   * Parse a json value into an array of data.
-   * @memberof CanvasEngine~utilities
-   * @param {string | Array} screenMap
-   * @returns {Array}
-   */
-  utilities.parseJsonArray = function(screenMap){
-    if (typeof(screenMap) === "string") {
-      screenMap = $.parseJSON(screenMap);
-    }
+export function parseJsonArray(screenMap){
+  if (typeof(screenMap) === "string") {
+    screenMap = $.parseJSON(screenMap);
+  }
+  if(isArray(screenMap)){
     for (var i = 0; i < screenMap.length; i++) {
       if ($.parseJSON(screenMap[i]) !== null) {
         screenMap[i] = $.parseJSON(screenMap[i]);
       }
     }
-    return (screenMap);
-  };
+  }
 
-  /**
-   * Set the properties on a thing when you don't know which properties to set and you have a lot of them.
-   * @param {object} thing The thing to set the properties on
-   * @param {object} params The properties to set and their data.
-   * @memberof CanvasEngine~utilities
-   */
-  utilities.setProperties = function(thing, params){
-    // Only set parameters that matter to the Component.
-    Object.keys(params).forEach(function(key){
-      if(thing.hasOwnProperty(key)) {
-        thing[key] = params[key];
-      }
-    });
-  };
-
-  CanvasEngine.utilities = utilities;
-
-})(window.CanvasEngine);
+  return (screenMap);
+}
