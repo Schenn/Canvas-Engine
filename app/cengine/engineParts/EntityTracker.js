c3220b585634513906cdf7fbd8102079a5ad667e
Steven Chennault <schenn@gmail.com>
2016-06-28 13:15:17 -0700

    Entity Tracker is now es6

--- app/cengine/engineParts/EntityTracker.js
+++ app/cengine/engineParts/EntityTracker.js
@@ -1,194 +1,190 @@
 /**
  * @author Steven Chennault <schenn@gmail.com>
  */
-(function(CanvasEngine){
+
+
+import privateProperties from "propertyDefinitions";
+import * as utilities from "utilities";
+
+import Entity from "../entities/Entity";
+/**
+ * The EntityTracker manages the information about all current living Entities in our engine.
+ *
+ * @class
+ * @memberOf CanvasEngine
+ * @inner
+ */
+class EntityTracker {
+
+  get maxZ(){
+    return privateProperties[this].maxZ;
+  }
+
+  get entityCount(){
+    return privateProperties[this].entities.size();
+  }
+
+  constructor(){
+    privateProperties[this].entities = new WeakMap();
+    privateProperties[this].entitiesByZ = [];
+    privateProperties[this].zExcludedFromInteractions = [];
+    privateProperties[this].maxZ = 0;
+  }
 
   /**
-   * The EntityTracker manages the information about all current living Entities in our engine.
+   * Add "Entities" to the Entity Tracker
    *
-   * @class
-   * @memberOf CanvasEngine
-   * @inner
-   * @static
+   * @param {CanvasEngine.Entities.Entity[]} entities
    */
-  var EntityTracker = function(){
-    var entities = {};
-    var entitiesByZ = [];
-    var zExcludedFromInteractions = [];
-    var maxZ = 0;
-
-    /**
-     * Add an array of entities to the entity tracker
-     *
-     * @param {Array.<Array.<CanvasEngine.Entity>>} ents
-     * @todo Thats a stupid sorting order, especially since Entities have z_index information.
-     *
-     **/
-    this.addEntities = function(ents){
-      $.each(ents, function(z, z_entities){
-        $.each(z_entities, function(index, entity){
-          entities[entity.name]=entity;
-          if(!CanvasEngine.utilities.exists(entitiesByZ[z])){
-            entitiesByZ[z] = [];
-            maxZ = z;
+  addEntities(entities){
+    for(let entity of entities){
+      if(entity instanceof Entity) {
+        privateProperties[this].entities.set(entity.name, entity);
+        if(!utilities.exists(privateProperties[this].entitiesByZ[entity.z_index])){
+          privateProperties[this].entitiesByZ[privateProperties[this].entities.get(entity.name).z_index] = new WeakSet();
+          if(privateProperties[this].entities.get(entity.name).z_index > privateProperties[this].maxZ){
+            privateProperties[this].maxZ = privateProperties[this].entities.get(entity.name).z_index;
           }
-          entitiesByZ[z].push(entity.name);
-        });
-      });
-    };
-
-    /**
-     * Prevent a z-index from interacting with things.
-     *
-     * Use this to make a z-index not search its objects for click and collision interactions
-     * Use to increase performance
-     *
-     * @param {Array.<number>} indexes The z-indexes to mark
-     * @param {boolean} invert Remove the mark from a z index if true
-     */
-    this.excludeZ = function(indexes, invert){
-      indexes.forEach(function(index){
-        if(!invert){
-          zExcludedFromInteractions[index] = true;
-        } else {
-          delete zExcludedFromInteractions[index];
         }
-      });
-    };
-
-    /**
-     * Get the entities with the given names
-     *
-     * @param {Array<string>} names array of names
-     * @returns {CanvasEngine.Entities.Entity[]}
-     */
-    this.getEntities = function(names){
-      var ents = [];
-      $.each(names, function(index, name){
-        // get the entity with the current name and add it to the list.
-        ents[index] = entities[name];
-      });
-
-      return ents;
-    };
-
-    /**
-     * Remove a collection of entities from the Tracker.
-     *
-     * @param {Array.<string>} names array of names
-     */
-    this.removeEntities = function(names){
-      $.each(names, function(index, name){
-        CanvasEngine.Screen.clear(entities[name]);
-        var z = entities[name].z_index;
-        var deleteIndex = -1;
-
-        // remove entity from the z index
-        entitiesByZ[z].forEach(function(value, index){
-          if(value === name){
-            deleteIndex = index;
-          }
-        });
-        if(deleteIndex >= 0){
-          delete entitiesByZ[z][deleteIndex];
-          // Remove the empty index
-          entitiesByZ[z] = CanvasEngine.utilities.cleanArray(entitiesByZ[z]);
-        }
-
-
-        // If no more entities occupy that z layer, tell the screen to remove the dispay for that z layer.
-        if(entitiesByZ[z].length === 0){
-          CanvasEngine.Screen.removeZLayer(z);
-          delete entitiesByZ[z];
-          entitiesByZ = CanvasEngine.utilities.cleanArray(entitiesByZ);
-
-          maxZ = Object.keys(entitiesByZ)[-1];
-        }
-        // remove entity from the entity list
-        delete entities[name];
-
-      });
-    };
-
-    /**
-     * Clear all the active entities
-     */
-    this.clearEntities = function(){
-      // Starting with the last z index, work backwards, clearing each collection
-      var zs = Object.keys(entitiesByZ);
-
-      for(var i = zs.length-1; i>=0; i--){
-        this.removeEntities(entitiesByZ[zs[i]]);
+        privateProperties[this].entitiesByZ[entity.z_index].add(entity.name);
       }
-    };
+    }
+  }
 
-    /**
-     * Get all the entities on a given z-index
-     *
-     * @param {number} z the Z index
-     * @returns {Array.<string>} Array of entity names
-     */
-    this.getEntitiesByZ = function(z){
-      var ents = [];
-      $.each(entitiesByZ[z], function(index, name){
-        ents.push(entities[name]);
+  /**
+   * Exclude (or stop excluding if you pass true as second argument) a set of z indexes from interactions
+   * @param {number[]} indices
+   * @param {boolean} invert
+   */
+  excludeZ(indices, invert){
+    for(let index of indices){
+      if(!invert){
+        privateProperties[this].zExcludedFromInteractions[index] = true;
+      } else {
+        delete privateProperties[this].zExcludedFromInteractions[index];
+      }
+    }
+
+  }
+
+  /**
+   * Get the entities with the given names
+   *
+   * @param {string[]} names array of names
+   * @returns {CanvasEngine.Entities.Entity[]}
+   */
+  getEntities(names){
+    var ents = [];
+    names.forEach((name, index)=>{
+      ents[index] = privateProperties[this].entities[name];
+    });
+
+    return ents;
+  }
+
+  /**
+   * Remove a collection of entities from the Tracker.
+   *
+   * @param {string[]} names array of names
+   */
+  removeEntities(names){
+    for(let name of names){
+      // Clear the entity from the Screen.
+
+      let entity= privateProperties[this].entities.get(name);
+
+      entity.messageToComponent("Renderer", "hide", ()=>{
+        privateProperties[this].entities.delete(name);
+        if(privateProperties[this].entitiesByZ[entity.z_index].size === 0){
+          privateProperties[this].entitiesByZ.splice(entity.z_index,1);
+          privateProperties[this].entitiesByZ = utilities.cleanArray(privateProperties[this].entitiesByZ);
+          let zs = privateProperties[this].entitiesByZ.keys();
+          privateProperties[this].maxZ = zs[zs.length -1];
+
+        }
+
       });
-      return ents;
-    };
+    }
+  }
 
-    /**
-     * Get the entities at a given pixel
-     *
-     * @param {coord} p The pixel coordinate
-     * @param {number} w The search area width
-     * @param {number} h The search area height
-     * @param {Array.<number>} zIndexes The indexes to search
-     * @param {string} [hasComponent] An optional Component to restrict your positions by.
-     *
-     * @returns {Array} The collection of found entities.
-     */
-    this.positionsAtPixel = function(p,w,h, zIndexes, hasComponent){
-      var positions = [];
-      for(var i =0; i< zIndexes.length; i++){
-        var z = zIndexes[i];
-        if (!(zExcludedFromInteractions[z])) {
-          var ents = this.getEntitiesByZ(z);
-          for (var j = 0; j < ents.length; j++) {
-            if(ents[j].getFromComponent("Renderer", "containsPixel",p)){
-              if (!CanvasEngine.utilities.exists(hasComponent)){
-                positions.push(ents[j]);
+  clearEntities(){
+    // Starting with the last z index, work backwards, clearing each collection
+    var zs = privateProperties[this].entitiesByZ.keys();
+
+    for(var i = zs.length-1; i>=0; i--){
+      this.removeEntities(privateProperties[this].entitiesByZ[zs[i]]);
+    }
+  }
+
+  /**
+   * Get all the entities on a given z-index
+   *
+   * @param {number} z the Z index
+   * @returns {CanvasEngine.Entities.Entity[]} Array of Entities
+   */
+  getEntitiesByZ(z){
+    var ents = [];
+    for(let name of privateProperties[this].entitiesByZ[z]){
+      ents.push(privateProperties[this].entities.get(name));
+    }
+    return ents;
+  }
+
+  /**
+   * Get the entities at a given pixel
+   *
+   * @param {coord} p The pixel coordinate
+   * @param {number} w The search area width
+   * @param {number} h The search area height
+   * @param {Array.<number>} zIndexes The indexes to search
+   * @param {string} [hasComponent] An optional Component to restrict your positions by.
+   *
+   * @returns {Array} The collection of found entities.
+   */
+  positionsAtPixel(p, w, h, zIndexes, hasComponent){
+    let positions = [];
+    for(let i =0; i< zIndexes.length; i++){
+      let z = zIndexes[i];
+      if (!(privateProperties[this].zExcludedFromInteractions[z])) {
+        for(let entity in this.getEntitiesByZ(z)) {
+          let containsPixel = false;
+          containsPixel = entity.getFromComponent("Renderer", "containsPixel", p);
+
+          if (!containsPixel) {
+            if (w > 1) {
+              let tempCoord = Object.assign({}, p);
+              for (let width = 1; width <= w; width++) {
+                tempCoord.x++;
+                containsPixel = entity.getFromComponent("Renderer", "containsPixel", tempCoord);
+                if (containsPixel) {
+                  break;
+                }
               }
-              else if(CanvasEngine.utilities.exists(hasComponent) && ents[j].hasComponent(hasComponent)){
-                positions.push(ents[j]);
-              }
-
-
             }
           }
+
+          if (!containsPixel) {
+            if (h > 1) {
+              let tempCoord = Object.assign({}, p);
+              for (let height = 1; height <= h; height++) {
+                tempCoord.y++;
+                containsPixel = entity.getFromComponent("Renderer", "containsPixel", tempCoord);
+                if (containsPixel) {
+                  break;
+                }
+              }
+            }
+          }
+          if (!utilities.exists(hasComponent) && containsPixel) {
+            positions.push(entity);
+          } else if (entity.hasComponent(hasComponent) && containsPixel) {
+            positions.push(entity);
+          }
         }
       }
+    }
+    return (positions);
+  }
+}
 
-      return (positions);
-    };
-
-    /**
-     * The current maximum z index
-     * @returns {number}
-     */
-    this.maxZ = function(){
-      return maxZ;
-    };
-
-    /**
-     * The number of entities being managed by the game.
-     *
-     * @returns {number}
-     */
-    this.entityCount = function(){
-      return Object.keys(entities).length;
-    };
-  };
-
-  // Attach the EntityTracker to the CanvasEngine.
-  CanvasEngine.EntityTracker = new EntityTracker();
-})(window.CanvasEngine);
+export default new EntityTracker();
