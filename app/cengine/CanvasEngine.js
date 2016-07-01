5ec8a55fdc05c96750d67d0842570fac1eeaef59
Steven Chennault <schenn@gmail.com>
2016-06-30 17:53:03 -0700

    CanvasEngine has been classed up. Next is integrating mocha tests.

--- app/cengine/CanvasEngine.js
+++ app/cengine/CanvasEngine.js
@@ -9,75 +9,79 @@
  * @typedef {Object.<string, number>} GeneralTypes~coords
  * @property {number} x
  * @property {number} y
-
  */
 
+import * as utilities from 'engineParts/utilities';
+import EntityManager from "engineParts/EntityManager";
+import EntityTracker from "engineParts/EntityTracker";
+import privateProperties from "engineParts/propertyDefinitions";
+import properties from "engineParts/propertyDefinitions";
+import ResourceManager from "engineParts/ResourceManager";
+import Screen from "engineParts/Screen";
 
-(function(){
-  /**
-   * @type {boolean}
-   * @default
-   */
-  var paused = true;
+import $ from "jQuery";
 
-  /**
-   * CanvasEngine contains the system.
-   *
-   * @namespace CanvasEngine
-   *
-   */
-  var CanvasEngine = {
-  };
+class CanvasEngine{
+
+  constructor(){
+    privateProperties[this].paused = true;
+
+    properties.observe("ResourceManager", ResourceManager, true, this);
+    properties.observe("EntityTracker", EntityTracker, true, this);
+
+    properties.observe("EntityManager", new EntityManager(this.ResourceManager, this.EntityTracker), true, this);
+    properties.observe("Screen", Screen, true, this);
+
+  }
 
   /**
    * Add a collection of entities from a json array.
    * @memberof CanvasEngine
-   * @param {Array} screenMap initialization data for the entities to add.
+   * @param {object[]} screenMap initialization data for the entities to add.
    * @param {boolean} start Start animating after adding entities.
    */
-  CanvasEngine.addMap = function(screenMap, start){
+  addMap(screenMap, start){
     // Convert to entity Array
 
     var entities = this.EntityManager.fromMap(screenMap);
 
     this.addEntities(entities, start);
-  };
+  }
 
   /**
    * Add an array of Entities
    *    (Entity being a class derived from the entity base class)
-   * @memberof CanvasEngine
-   * @param {Array} entities The Entities to add, grouped by z-index
-   * @param {boolean} start Start animating after adding entities
    *
-   * @todo Change the expected sort structure of entities
+   * @param {Entity[]} entities
+   * @param {boolean} start
    */
-  CanvasEngine.addEntities = function(entities, start){
-    $.each(entities, function(z){
-      CanvasEngine.Screen.addZLayer(z);
-    });
-
+  addEntities(entities, start){
     this.EntityTracker.addEntities(entities);
+    this.Screen.addZLayers(this.EntityTracker.zIndexes);
 
     if(start){
-      paused = false;
-      this.Loop();
+      privateProperties[this].paused = false;
     }
-  };
+  }
 
   /**
-   * Run Forever
+   * Clear all entities from the system
    * @memberof CanvasEngine
-   *
-   * @private
    */
-  CanvasEngine.Loop = function(){
-    var self = this;
-    requestAnimationFrame(function(){
-      self.Screen.drawScreen();
-      self.Loop();
-    });
-  };
+  clearEntities(){
+    //noinspection JSUnusedAssignment
+    paused = true;
+    this.EntityTracker.clearEntities();
+    paused = false;
+  }
+
+  render(){
+    let zs = this.EntityTracker.zIndexes;
+
+    for(let z = zs; z >0; z--){
+      this.drawZ(z);
+    }
+  }
 
   /**
    * Draw through a z-index.
@@ -89,52 +93,35 @@
    *
    * @memberof CanvasEngine
    * @param {number} z The z index to draw
-   * @param {jQuery#enhancedContext} ctx The EnhancedContext of a canvas
    */
-  CanvasEngine.drawZ = function(z, ctx){
-    if(!paused && CanvasEngine.EntityTracker.entityCount() > 0) {
-      setTimeout(function(){
-        $.each(CanvasEngine.EntityTracker.getEntitiesByZ(z), function (index, entity) {
-          if(CanvasEngine.utilities.exists(entity)){
-            entity.broadcastToComponents("update");
-            if(entity.getFromComponent("Renderer", "isDirty")){
-              entity.messageToComponent("Renderer", "clear", ctx);
-              entity.messageToComponent("Renderer", "render", ctx);
-            }
-            entity.broadcastToComponents("postRender");
+  drawZ(z){
+    let ctx = this.Screen.getScreenContext(z);
+    if(!privateProperties[this].paused && this.EntityTracker.entityCount() > 0) {
+      setTimeout(()=>{
+        for(let entity of this.EntityTracker.getEntitiesByZ(z)){
+          entity.broadcastToComponents("update");
+          if(entity.getFromComponent("Renderer", "isDirty")){
+            entity.messageToComponent("Renderer", "clear", ctx);
+            entity.messageToComponent("Renderer", "render", ctx);
           }
-        });
+          entity.broadcastToComponents("postRender");
+        }
       },1);
     }
-  };
+  }
 
-  /**
-   * Clear all entities from the system
-   * @memberof CanvasEngine
-   */
-  CanvasEngine.clearEntities = function(){
-    //noinspection JSUnusedAssignment
-    paused = true;
-    CanvasEngine.EntityTracker.clearEntities();
-    paused = false;
-  };
+  Loop(){
+    requestAnimationFrame(()=>{
+      this.render();
+      this.Loop();
 
+    });
+  }
 
-  /**
-   * Tell every entity under the current coords that the mouse is interacting with it.
-   *
-   * Called by the Screen when a mouse interacts with the stack.
-   *
-   * @param {GeneralTypes~coords} coords The click position
-   * @param {string} interaction the type of mouse interaction
-   * @param {GeneralTypes~coords} [previous] The Previous mouse position
-   * @memberof CanvasEngine
-   *
-   */
-  CanvasEngine.mouse = function(coords, interaction, previous){
-    setTimeout(function(){
-      var ents = CanvasEngine.positionsAtPixel(coords, 1, 1, "Mouse");
-      $.each(ents, function(index, ent){
+  onMouse(coords, interaction, previous){
+    setTimeout(()=>{
+      let ents = this.positionsAtPixel(coords, 1, 1, "Mouse");
+      for(let ent of ents){
         switch(interaction){
           case "MouseMove":
             ent.broadcastToComponents("onMouseOver", coords);
@@ -153,22 +140,20 @@
             console.log ("I don't know how to handle "+interaction);
             break;
         }
+      }
 
-      });
+      if(utilities.exists(previous) && interaction === "MouseMove"){
+        let last = this.positionsAtPixel(previous, 1, 1, "Mouse");
+        // for each entity in last, if they are not in ents, then trigger onMouseOut
 
-    if(CanvasEngine.utilities.exists(previous) && interaction === "MouseMove"){
-      var last = CanvasEngine.positionsAtPixel(previous, 1, 1, "Mouse");
-      // for each entity in last, if they are not in ents, then trigger onMouseOut
-      $.each(last, function(index, lastEnt){
-        if(!ents.includes(lastEnt)){
-          lastEnt.broadcastToComponents("onMouseOut");
+        for(let lastEnt of last){
+          if(!ents.includes(lastEnt)){
+            lastEnt.broadcastToComponents("onMouseOut");
+          }
         }
-      });
-    }
+      }
     },1);
-  };
-
-
+  }
 
   /**
    * Pause the game
@@ -176,42 +161,42 @@
    *
    * @param {boolean} forceIt
    */
-  CanvasEngine.pause = function(forceIt){
+  pause(forceIt){
     paused = (CanvasEngine.utilities.exists(forceIt)) ? forceIt : !paused;
 
     if(!paused){
       this.Loop();
     }
-  };
+  }
 
   /**
    * Pause the engine on a right click.
    * @memberof CanvasEngine
    *
    */
-  CanvasEngine.pauseOnRightClick = function(){
+  pauseOnRightClick(){
     this.pause(true);
-  };
+  }
 
   /**
    * Unpause the engine
    * @memberof CanvasEngine
    */
-  CanvasEngine.unPause = function(){
+  unPause(){
     $(document).off(".unpause");
     $(window).off(".unpause");
 
     this.pause(false);
-  };
+  }
 
   /**
    * Is the engine currently paused?
    * @memberof CanvasEngine
    * @returns {boolean}
    */
-  CanvasEngine.isPaused = function(){
+  isPaused(){
     return paused;
-  };
+  }
 
   /**
    * Get all the entities at a given pixel
@@ -223,14 +208,14 @@
    * @param {string} [hasComponent] Require entities to have a specific component.
    * @returns {Array} Entities at the specified position
    */
-  CanvasEngine.positionsAtPixel = function(p, w, h, hasComponent){
-    var zPixels =CanvasEngine.Screen.atPixel(p.x, p.y, h, w, true);
+  positionsAtPixel(p, w, h, hasComponent){
+    let zPixels =this.Screen.atPixel(p.x, p.y, h, w, true);
     if (zPixels.length > 0) {
-      return CanvasEngine.EntityTracker.positionsAtPixel(p,w,h, Object.keys(zPixels), hasComponent);
+      return this.EntityTracker.positionsAtPixel(p,w,h, Object.keys(zPixels), hasComponent);
     } else {
       return [];
     }
-  };
+  }
 
   /**
    * Setup the canvas engine.
@@ -239,22 +224,23 @@
    * @param {jQuery | HTMLElement} canvas The canvas to base the engine off of
    *
    */
-  CanvasEngine.setup = function(canvas){
-    CanvasEngine.Screen.setScreen(canvas);
-    $(document).on("contextmenu", function(){
-      CanvasEngine.pauseOnRightClick();
+  setup(canvas){
+    this.Screen.setScreen(canvas);
+    $(document).on("contextmenu", ()=>{
+      this.pauseOnRightClick();
 
-      $(document).on("keypress.unpause", function(e){
+      $(document).on("keypress.unpause", (e)=>{
         if(e.keyCode === 27){
-          CanvasEngine.unPause();
+          this.unPause();
         }
       });
-      $(document).on("click.unpause", CanvasEngine.unPause);
-      $(window).on("blur.unpause", CanvasEngine.unPause);
+      $(document).on("click.unpause", this.unPause);
+      $(window).on("blur.unpause", this.unPause);
 
     });
 
-  };
+  }
 
-  window.CanvasEngine = CanvasEngine;
-})();
+}
+
+window.CanvasEngine = new CanvasEngine();
