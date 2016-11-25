44377fb59d5c95d6c8417eb0df99f0f57d5cf85c
Steven Chennault <schenn@gmail.com>
2016-11-25 15:04:47 -0800

    Return a promise if not finished generating sprites
    When you ask for a sprite, if the SpriteSheet isn't ready to give you one,
    it will instead return a promise.  There is an interval check that is cleared
    and resolves the promise to the sprite or throws if the sprite is missing once
    the spritesheet is finished processing.

--- app/cengine/resources/SpriteSheet.js
+++ app/cengine/resources/SpriteSheet.js
@@ -11,7 +11,7 @@ import {properties} from "engineParts/propertyDefinitions.js";
 
 const privateProperties = new WeakMap();
 
-let processSprites = function(spriteCache, source, spriteWidth, spriteHeight){
+let processSprites = function(spriteCache, source, spriteWidth, spriteHeight, onFinished){
   let sx = 0, sy= 0, index=0;
   let useCache = utilities.exists(spriteCache);
 
@@ -29,23 +29,27 @@ let processSprites = function(spriteCache, source, spriteWidth, spriteHeight){
     sy += spriteHeight;
     sx = 0;
   }
-
+  onFinished();
   return sprites;
 };
 
 // If sprites are an object, take their data and fill in the rest.
-let processSpriteObject = function(spriteCache, spriteWidth, spriteHeight) {
-  let sprites = {};
-  for(let name of Object.keys(spriteCache)){
-    let sprite = spriteCache[name];
-    sprites[name] = Object.assign({}, {width: spriteWidth, height: spriteHeight}, sprite);
+let processSpriteObject = function(spriteCache, spriteWidth, spriteHeight, onFinished) {
+  var sprites = {};
+  for(var name of Object.keys(spriteCache)){
+    console.log(name);
+    console.log(spriteCache[name]);
+    sprites[name] = Object.assign({}, {width: spriteWidth, height: spriteHeight}, spriteCache[name]);
   }
-
+  onFinished();
   return sprites;
 };
 
 /**
  * The SpriteSheet Resource manages creating and accessing specific sprites from its source image
+ *
+ * Wait until your image is finished loading to set the SpriteSheet source so that the SpriteSheet can properly generate its sprites.
+ *  If you use the ResourceManager, than it is handled for you.
  *
  * @class
  * @memberOf CanvasEngine.Resources
@@ -57,65 +61,88 @@ let processSpriteObject = function(spriteCache, spriteWidth, spriteHeight) {
 export class SpriteSheet {
 
   /**
+   * Sprite Height
+   *
    * @returns {number}
    */
   get sHeight(){
-    return privateProperties[this].spriteHeight;
+    return privateProperties[this.id].spriteHeight;
   }
 
   /**
+   * Sprite Height
    * @param {number} height
    */
   set sHeight(height){
-    privateProperties[this].spriteHeight = height;
+    privateProperties[this.id].spriteHeight = height;
   }
 
   /**
+   * Sprite Width
+   *
    * @returns {number}
    */
   get sWidth(){
-    return privateProperties[this].spriteWidth;
+    return privateProperties[this.id].spriteWidth;
   }
 
   /**
+   * Sprite Width
+   *
    * @param {number} width
    */
   set sWidth(width){
-    privateProperties[this].spriteWidth = width;
+    privateProperties[this.id].spriteWidth = width;
   }
 
   /**
+   * SpriteSheet Source Image
+   *
    * @returns {Image}
    */
   get Source(){
-    return privateProperties[this].source.src;
+    return privateProperties[this.id].source.src;
+  }
+
+  get Sprites(){
+    return privateProperties[this.id].sprites;
   }
 
   /**
+   * SpriteSheet Source Image
+   *
    * @param {Image} source
    */
   set Source(source){
-    if(privateProperties[this].source === ""){
+    if(privateProperties[this.id].source === ""){
       if(source instanceof Image) {
         this.processSprites(source);
       } else {
         throw "Source needs to be a js Image object";
       }
     } else {
-      throw "Source already set. Create new SpriteSheet instead."
+      throw "Source already set. Create new SpriteSheet instead.";
     }
   }
 
   constructor(details){
-    privateProperties[this]={};
-    if(!utilities.exists(details.height) || !utilities.exists(details.width)) throw "Missing the width or height of the sprites";
-    privateProperties[this].spriteHeight = details.height;
-    privateProperties[this].spriteWidth = details.width;
-    privateProperties[this].sprites = [];
-    privateProperties[this].source = "";
-    privateProperties[this].isBuilding = false;
 
-    privateProperties[this].spriteCache = (utilities.exists(details.sprites)) ? details.sprites : null;
+    let id = (details.source || utilities.randName());
+
+    Object.defineProperties(this, {
+      id:properties.lockedProperty(id)
+    });
+
+    privateProperties[this.id]={};
+    if(!utilities.exists(details.height) || !utilities.exists(details.width)) throw "Missing the width or height of the sprites";
+    privateProperties[this.id].spriteHeight = details.height;
+    privateProperties[this.id].spriteWidth = details.width;
+    privateProperties[this.id].sprites = [];
+    privateProperties[this.id].source = "";
+    privateProperties[this.id].isProcessing = false;
+    privateProperties[this.id].onSpritesLoaded = (utilities.exists(details.onSpritesLoaded)) ? details.onSpritesLoaded : null;
+
+    privateProperties[this.id].spriteCache = (utilities.exists(details.sprites)) ? details.sprites : null;
 
     if(utilities.exists(details.source)){
       this.Source = details.source;
@@ -124,31 +151,68 @@ export class SpriteSheet {
 
   /**
    * Process the sprites against an image.
+   *
    * @param {Image} img
    */
-  processSprites(img){
-    privateProperties[this].source = img;
-    privateProperties[this].isBuilding = true;
-    // If we have a spriteCache and it's an object, not an array
-    if(utilities.exists(privateProperties[this].spriteCache) &&
-      Object.keys(privateProperties[this].spriteCache).length > 0 &&
-      !utilities.isArray(privateProperties[this].spriteCache)){
+  processSprites(img)
+  {
+    privateProperties[this.id].isProcessing = true;
+    privateProperties[this.id].source = img;
+    let work = ()=>{
+      // If we have a spriteCache and it's an object, not an array
+      if(utilities.exists(privateProperties[this.id].spriteCache) &&
+        Object.keys(privateProperties[this.id].spriteCache).length > 0 &&
+        !utilities.isArray(privateProperties[this.id].spriteCache)){
 
-      privateProperties[this].sprites = processSpriteObject(privateProperties[this].spriteCache,  privateProperties[this].spriteHeight,privateProperties[this].spriteWidth);
-    }
-    else{
-      privateProperties[this].sprites = processSprites( privateProperties[this].spriteCache, privateProperties[this].source, privateProperties[this].spriteHeight,privateProperties[this].spriteWidth);
+        privateProperties[this.id].sprites = processSpriteObject(privateProperties[this.id].spriteCache,  privateProperties[this.id].spriteHeight,privateProperties[this.id].spriteWidth, ()=>{
+          privateProperties[this.id].isProcessing = false;
+        });
+      }
+      else{
+        privateProperties[this.id].sprites = processSprites( privateProperties[this.id].spriteCache, privateProperties[this.id].source, privateProperties[this.id].spriteHeight,privateProperties[this.id].spriteWidth, ()=>{
+          privateProperties[this.id].isProcessing = false;
+        });
+      }
+
+    };
+    if(img.complete){
+      work();
+
+    } else {
+      img.addEventListener("load", work);
     }
   }
 
   /**
    * Get a sprite detail object by its name
+   *
    * @param {string} name
    * @returns {{sx: number, sy: number, sWidth: number, sHeight: number}}
    * @todo Wait for spritesheet to finish building
    */
   getSprite(name){
-    if(!utilities.exists(privateProperties[this].sprites[name])) throw "Sprite: " +name + " Not Found on SpriteSheet: "+ this.Source;
-    return privateProperties[this].sprites[name];
+    if(privateProperties[this.id].isProcessing){
+      console.log("Waiting for spritesheet to finish processing.");
+      let self = this;
+      return new Promise((resolve, reject)=>{
+        let int= setInterval(()=>{
+          if(privateProperties[self.id].isProcessing === false){
+            clearInterval(int);
+            if(!utilities.exists(privateProperties[self.id].sprites[name])){
+              throw "Sprite: " +name + " Not Found on SpriteSheet: "+ self.Source;
+            } else {
+              resolve(privateProperties[self.id].sprites[name]);
+            }
+          }
+        }, 1000);
+
+      });
+    }
+
+    if(!utilities.exists(privateProperties[this.id].sprites[name])){
+      throw "Sprite: " +name + " Not Found on SpriteSheet: "+ this.Source;
+    }
+
+    return privateProperties[this.id].sprites[name];
   }
 }
--- tests/resources/SpriteSheetTests.js
+++ tests/resources/SpriteSheetTests.js
@@ -1,33 +1,51 @@
 /**
  * Created by schenn on 8/6/16.
  */
-SystemJS.import('resources/SpriteSheet.js').then(function(m) {
+import {SpriteSheet} from "resources/SpriteSheet.js";
+
+let objectSheet = new SpriteSheet({
+  height: 50, width: 50,
+  source: new Image("../images/og_image_2.jpg"),
+  sprites: {
+    a: {
+      x: 200, y: 200
+    }
+  }
+});
+
   /**
    * We're testing the Base Component object
    * @module CanvasEngine/tests/EngineParts/EntityManager
    */
-  QUnit.module("Resources/SpriteSheet");
+  QUnit.module("Resources/SpriteSheet", {before: function(){
+    this.image1 = new Image();
+    this.image1.src = "../images/og_image_1.jpg";
+
+    this.image2 = new Image();
+    this.image2.src = "../images/og_image_2.jpg";
+
+  }});
 
   /**
    * Ensure that we can't create a SpriteSheet without properly constructed arguments
    */
   QUnit.test("SpriteSheet requires arguments to construct", function(assert){
     assert.throws(function(){
-      let sheet = new m.SpriteSheet();
+      let sheet = new SpriteSheet();
     }, undefined, "No arguments provided");
 
     assert.throws(function(){
-      let sheet = new m.SpriteSheet({});
+      let sheet = new SpriteSheet({});
     }, /Missing/, "SpriteSheet won't construct without a provided sprite height and width");
 
     assert.ok(function(){
-      let sheet = new m.SpriteSheet({height:50, width:50});
+      let sheet = new SpriteSheet({height:50, width:50});
     }, "Sheet constructs ok with just a height and width.");
 
     assert.ok(function(){
       let image = new Image();
       image.src = "../images/og_image_1.jpg";
-      let sheet = new m.SpriteSheet({height:50, width:50, source:image});
+      let sheet = new SpriteSheet({height:50, width:50, source:image});
     }, "Sheet constructs ok with a height, width, and source.");
 
   });
@@ -38,16 +56,12 @@ SystemJS.import('resources/SpriteSheet.js').then(function(m) {
   QUnit.test("SpriteSheet can process a collection of sprites", function(assert){
 
     assert.ok(function(){
-      let image = new Image();
-      image.src = "../images/og_image_1.jpg";
-      let sheet = new m.SpriteSheet({height: 50, width: 50});
-      sheet.source = image;
+      let sheet = new SpriteSheet({height: 50, width: 50});
+      sheet.source = this.image1;
     }, "SpriteSheet will take a source after construct, then use that source to generate its sprites");
 
     assert.ok(function(){
-      let image = new Image();
-      image.src = "/app/images/og_image_1.jpg";
-      let sheet = new m.SpriteSheet({height: 50, width: 50, source: image});
+      let sheet = new SpriteSheet({height: 50, width: 50, source: this.image1});
     }, "SpriteSheet doesn't fail to load with an image, regardless of whether the image has finished loading or not.");
   });
 
@@ -57,17 +71,13 @@ SystemJS.import('resources/SpriteSheet.js').then(function(m) {
   QUnit.test("SpriteSheet source is protected from pollution", function(assert){
 
     assert.throws(function(){
-      let image = new Image();
-      image.src = "../images/og_image_1.jpg";
-      let image2 = new Image();
-      image2.src = "../images/og_image_2.jpg";
 
-      let sheet = new m.SpriteSheet({height: 50, width: 50, source: image});
-      sheet.Source = image2;
+      let sheet = new SpriteSheet({height: 50, width: 50, source: this.image1});
+      sheet.Source = this.image2;
     }, /Source/, "SpriteSheet throws if you try to change its source.");
 
     assert.throws(function(){
-      let sheet = new m.SpriteSheet({height: 50, width: 50, source: ""});
+      let sheet = new SpriteSheet({height: 50, width: 50, source: ""});
     }, /js/, "SpriteSheet throws if the provided source isn't an Image object");
   });
 
@@ -76,20 +86,6 @@ SystemJS.import('resources/SpriteSheet.js').then(function(m) {
    * Ensure we can retrieve sprites from a SpriteSheet.
    */
   QUnit.test("SpriteSheet contains accessible sprites!", function (assert){
-    let img = new Image();
-    img.src = "../images/og_image_1.jpg";
-    let sheet = new m.SpriteSheet({height: 50, width: 50, source: img});
-
-    assert.propEqual(sheet.getSprite(0), {x: 0, y: 0, width: 50, height: 50}, "We get a sprite data object from the spritesheet.");
-
-    let sheet2 = new m.SpriteSheet({height: 50, width: 50, source: img, sprites: {
-      a: {
-        x: 200, y: 200
-      }
-    }});
-
-    assert.propEqual(sheet2.getSprite('a'), {height: 50, width: 50, x: 200, y: 200}, "We can specify specific names for sprites.");
-
-    assert.throws(function(){sheet.getSprite(0)}, /Found/, "Using a dictionary of sprite data prevents the auto-generation of sprites.");
+    assert.throws(function(){objectSheet.getSprite(0);}, /Found/, "Using a dictionary of sprite data prevents the auto-generation of sprites.");
+    assert.propEqual(objectSheet.getSprite('a'), {height: 50, width: 50, x: 200, y: 200}, "If we wait for the sprites to finish generating, we can specify specific names for sprites.");
   });
-});
