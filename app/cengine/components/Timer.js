80b9519770c1e2925c0edc10d9a1bac310478993
Steven Chennault <schenn@gmail.com>
2016-12-09 18:25:03 -0800

    Changed onBeep to onElapsed. Changed onElapsed to only fire once. Fixed issue with 'next time'

--- app/cengine/components/Timer.js
+++ app/cengine/components/Timer.js
@@ -8,8 +8,8 @@
  * @property {function} [onElapsed]
  */
 
-import {Component} from "components/Component.js";
-import * as utilities from "engineParts/utilities.js";
+import {Component} from "../components/Component.js";
+import * as utilities from "../engineParts/utilities.js";
 
 let privateProperties = new WeakMap();
 
@@ -47,10 +47,10 @@ export class Timer extends Component{
     privateProperties[this.id].delta = new Date();
     privateProperties[this.id].isActive = true;
 
-    privateProperties[this.id].onBeep = utilities.isFunction(params.onElapsed) ? params.onElapsed : null;
+    privateProperties[this.id].onElapsed = utilities.isFunction(params.onElapsed) ? params.onElapsed : null;
     privateProperties[this.id].onUpdate = utilities.isFunction(params.onUpdate) ? params.onUpdate : null;
     privateProperties[this.id].timeUntilBeep = utilities.exists(params.duration) ? params.duration : 0;
-    privateProperties[this.id].beep = privateProperties[this.id].date.getDate() + privateProperties[this.id].timeUntilBeep;
+    privateProperties[this.id].beep = privateProperties[this.id].date.getTime() + privateProperties[this.id].timeUntilBeep;
 
   }
 
@@ -62,17 +62,20 @@ export class Timer extends Component{
       privateProperties[this.id].delta = privateProperties[this.id].date;
       privateProperties[this.id].date = new Date();
       // Only beep if we have all the information we need to beep.
-      if (utilities.isFunction(privateProperties[this.id].onBeep) &&
+      if (utilities.isFunction(privateProperties[this.id].onElapsed) &&
         privateProperties[this.id].timeUntilBeep > 0 &&
         this.MS >= privateProperties[this.id].beep) {
 
-        privateProperties[this.id].onBeep(this.deltaTime);
-        privateProperties[this.id].beep = this.MS+ privateProperties[this.id].timeUntilBeep;
+        privateProperties[this.id].onElapsed(this.deltaTime);
+
+        delete privateProperties[this.id].onElapsed;
       }
 
       if (utilities.isFunction(privateProperties[this.id].onUpdate)) {
         privateProperties[this.id].onUpdate(this.deltaTime);
+        privateProperties[this.id].beep = this.MS+ privateProperties[this.id].timeUntilBeep;
       }
+
     }
   }
 
