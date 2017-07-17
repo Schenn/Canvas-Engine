9a05a3f478bf1d76643ef876d1963b5738c7fd34
schennault <schenn@gmail.com>
2017-07-16 21:44:12 -0700

    Replace jquery with more vanilla

--- app/cengine/jqueryPlugins/jQueryDocFile.js
+++
@@ -1,9 +0,0 @@
-/**
- * @author Steven Chennault <schenn@gmail.com>
- * @external jQuery
- * @see {@link http://learn.jquery.com/plugins/|jQuery Plugins}
- */
-/**
- * @namespace fn
- * @memberof external:jQuery
- */
